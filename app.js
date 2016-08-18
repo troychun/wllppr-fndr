47a25b6703f270bd950d0c1164cf6a60623f8148
Remote Prep <remoteprep@Jesss-MacBook-Pro.local>
2016-08-17 21:28:09 -0700

    wrapped ajax in a function, also added search term to parameters

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 $(function() {
-        var params = {
-            // Request parameters
-            "q": "bird",
-            "count": "10",
-            "offset": "0",
-            "mkt": "en-us",
-            "safeSearch": "Moderate",
-        };
+    var params = {
+        // Request parameters
+        "q": "dog",
+        "count": "10",
+        "offset": "0",
+        "mkt": "en-us",
+        "safeSearch": "Moderate",
+    };
 
+    function searchBing() {
         $.ajax({
-            url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","c92a135e4f8e4d71a99076a9877f2258");
-            },
-            type: "GET",
-            // Request body
-            data: "{body}",
-        })
-        .done(function(data) {
-            console.log(data);
-        	$('#searchResults').html("<img src=" + data.value[0].contentUrl + ">");
-        })
-        .fail(function() {
-            console.log('error');
-        });
+                url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(params),
+                beforeSend: function(xhrObj) {
+                    // Request headers
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "c92a135e4f8e4d71a99076a9877f2258");
+                },
+                type: "GET",
+                // Request body
+                data: "{body}",
+            })
+            .done(function(data) {
+                console.log(data);
+                $('#searchResults').html("<img src=" + data.value[0].contentUrl + ">");
+            })
+            .fail(function() {
+                console.log('error');
+            });
+    }
+
+    $("#button").on('click', function(event) {
+        event.preventDefault();
+        var searchTerm = $('#mySearch').val();
+        params.q = searchTerm;
+				searchBing();
     });
-	$("#button").on('click', function(event){
-		event.preventDefault();
-		var searchTerm = $('#mySearch').val();
-		console.log(searchTerm);
-	});
+});
+
 
 
 //c92a135e4f8e4d71a99076a9877f2258
